47a04cb06ac8db478335d22dbc7d582a6528e760
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-07 14:19:37 +0500

    Minor layout and page modification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Navbar from './components/header'
-import Wrapper from './components/shared/wrapper'
-import ProductList from './components/productList'
-import MainPage from './components/home'
-import Footer from './components/footer'
+import Navbar from '@/app/components/header'
+import Footer from '@/app/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,18 +21,10 @@ export default function RootLayout({
         <header>  
           <Navbar />
         </header>
-        <div className="font-menu bg-white text-black">
-          <Wrapper>
-            <main>
-              {/* <MainPage /> */}
-              <ProductList />
-            </main>
-            {children}
-          </Wrapper>
-          <footer>
-            <Footer />
-          </footer>
-        </div>
+        {children}
+        <footer>
+          <Footer />
+        </footer>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import Wrapper from './components/shared/wrapper'
+import MainPage from './components/home'
+import ProductList from './components/productList'
+
 export default function Home() {
   return (
-    <div>
+    <div className="font-menu bg-white text-black">
+      <Wrapper>
+        <main>
+          {/* <MainPage /> */}
+          <ProductList />
+        </main>
+      </Wrapper>
     </div>
   )
 }
