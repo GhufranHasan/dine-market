698d4f35a5cb1cd2b03ba15d9b62cbc6881dee6f
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-15 10:56:17 +0500

    Modification of Product deatls

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -21,16 +21,16 @@ export default function Page({ params }: { params: { id: string } }) {
   const result = getProjectDetails(params.id);
 
   return (
-    <div className="flex justify-evenly mt-16 py-10 flex-wrap">
+    <div className="flex justify-between mt-16 py-10 flex-wrap">
       {result.map((product: Product) => (
-        <div key={product.itemId} className="flex justify-between gap-6">
-          <div>
+        <div key={product.itemId} className="flex justify-evenly gap-6 w-full">
+          <div className="sm:h-full">
             <Image src={product.imageUrl} alt={product.imageText} width={400} height={400} />
           </div>
-          <div>
+          <div className="w-full md:w-[80%]">
             <div>
               <h1 className="text-2xl">{product.title}</h1>
-              <h2 className="text-base font-semibold text-center text-gray-400">{product.category}</h2>
+              <h2 className="text-base font-semibold text-gray-400">{product.category}</h2>
             </div>
             <div>
               <h3 className="mt-6 text-xs font-semibold">SELECT SIZE</h3>
@@ -46,8 +46,8 @@ export default function Page({ params }: { params: { id: string } }) {
                 <h3 className="text-[10px] font-semibold">Quantity:</h3>
                 <Quantity />
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex bg-black h-20 md:w-[60%] w-full md:h-16 py-3 my-9 text-center justify-evenly space-x-2">
+              <div className="flex items-center space-x-4">
+                <div className="flex bg-black h-20 md:w-[15%] w-full md:h-14 py-3 px-2 my-9 text-center justify-evenly space-x-2">
                   <BiCart className="text-white text-2xl my-auto md:text-2xl" />
                   <button className="text-white text-sm font-semibold py-1 md:py-2" type="submit">Add To Cart</button>
                 </div>
