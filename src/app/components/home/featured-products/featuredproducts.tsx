1e38b1ff455208ae2f9783fcac7a04b1c8ddffbd
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-21 16:28:33 +0500

    Swiper added and customized

--- src/app/components/home/featured-products/featuredproducts.tsx
+++ src/app/components/home/featured-products/featuredproducts.tsx
@@ -1,9 +1,20 @@
+"use client";
 import React from 'react';
-import Link from 'next/link';
+import SwiperCore from 'swiper';
+import { Navigation, Pagination, A11y } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './slider-arrows.css';
+
 import Commonhead from '../../shared/commonhead';
 import Cardproduct from '../../shared/cardproduct';
 import { ProductCategory, Product } from '@/app/utils/type';
-import  productData from '../../../data/productData';
+import productData from '../../../data/productData';
+
+// Install Swiper components
+SwiperCore.use([Navigation, Pagination, A11y]);
 
 const Featuredproducts = () => {
   // Filter the featured products from the productData array
@@ -15,19 +26,27 @@ const Featuredproducts = () => {
     <div className="font-bold">
       <Commonhead bluehead="PRODUCTS" mainhead="Check What We Have" />
       <div className="md:flex">
-        {/* Featured Products */}
-        {featuredProducts.map((product: Product) => (
-          <div key={product.itemId}>
-            <Cardproduct
-              id={product.itemId}
-              imageUrl={product.imageUrl}
-              alterText={product.imageText}
-              prodTitle={product.title}
-              prodCategory={product.category}
-              prodPrice={product.price}
-            />
-          </div>
-        ))}
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          // pagination={{ clickable: true }}
+          loop
+        >
+          {/* Featured Products */}
+          {featuredProducts.map((product: Product) => (
+            <SwiperSlide key={product.itemId}>
+              <Cardproduct
+                id={product.itemId}
+                imageUrl={product.imageUrl}
+                alterText={product.imageText}
+                prodTitle={product.title}
+                prodCategory={product.category}
+                prodPrice={product.price}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
