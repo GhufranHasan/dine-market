e9587cb901ed1143fc29d3a047a80f4881d05a66
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-24 00:00:08 +0500

    Not checked yet.

---
+++ src/app/components/home/productsCarousel/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsCarousel = () => {
+  return (
+    <div>ProductsCarousel</div>
+  )
+}
+
+export default ProductsCarousel
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import Wrapper from './components/shared/wrapper'
 import MainPage from './components/home'
+import BASE_PATH_API from './shared/BasePath';
+import ProductsCarousel from './components/home/productsCarousel';
 
-export default function Home() {
+async function fetchAllProductsData() {
+  let res = await fetch(`${BASE_PATH_API}/api/products`);
+
+  if(!res.ok)
+    throw new Error("Failed to fetch")
+
+  return res.json();
+}
+
+export default async function Home() {
+  let response = await fetchAllProductsData();
+  console.log("response: ", response)
   return (
     <div className="font-menu bg-white text-black">
       <Wrapper>
         <main>
-          <MainPage />
+          {/* <MainPage /> */}
+          <ProductsCarousel />
         </main>
       </Wrapper>
     </div>
