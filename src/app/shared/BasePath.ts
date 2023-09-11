463d77ffa09a93091dae7400757d14a82e032161
Ghufran Hasan <ghufran.aims@gmail.com>
2023-09-11 18:18:50 +0500

    BasePath - Updated

--- src/app/shared/BasePath.ts
+++ src/app/shared/BasePath.ts
@@ -1,3 +1,3 @@
-const BASE_PATH_API = process.env.NODE_ENV === "development" ? "http://localhost:3000" : ""
+const BASE_PATH_API = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://dine-market-ochre.vercel.app/"
 
 export default BASE_PATH_API 
